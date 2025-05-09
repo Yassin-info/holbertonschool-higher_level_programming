2a967ee7b319b077dae9e8270431c569867f054b
yassin74 <yassin.jaghmim@gmail.com>
2025-05-09 12:35:40 +0000

    fix error

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
-     if b == 0:
+    if b == 0:
         return 1
     elif b > 0:
-    resultat = 1
+        resultat = 1
         for i in range(b):
             resultat *= a
         return resultat
@@ -12,3 +12,4 @@ def pow(a, b):
         for i in range(abs(b)):
             resultat *= (a)
     return 1/resultat
+    
