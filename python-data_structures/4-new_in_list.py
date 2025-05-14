b3c471222c72896363040f4095ed5d2171de364a
yassin74 <yassin.jaghmim@gmail.com>
2025-05-14 08:52:36 +0000

    fix errors in local

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    nmy_list = my_list.copy()
+   copy_list = my_list.copy()
     if idx < 0 or idx >= len(my_list):
-        return nmy_list
+        return copy_list
+    copy_list[idx] = element 
+    return copy_list
