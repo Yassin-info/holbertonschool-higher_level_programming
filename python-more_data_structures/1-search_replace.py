b86881ce0f4f4034ed01baddb29c95cfcb8c428f
yassin74 <yassin.jaghmim@gmail.com>
2025-05-15 07:16:09 +0000

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,4 +4,6 @@ def search_replace(my_list, search, replace):
     for item in my_list:
         if item == search:
             new_list.append(replace)
+        else:
+            new_list.append(item)
     return new_list
