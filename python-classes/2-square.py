e463d955ebc1e5d939b9c086a9b776ff1dc0beec
yassin74 <yassin.jaghmim@gmail.com>
2025-05-21 10:33:59 +0000

    add the doc

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,27 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class used to represent a square shape.
+
+Classes:
+    Square: Represents a square with a private size attribute.
+"""
+
+
 class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
     def __init__(self, size=0):
+        """
+        Initializes a Square instance with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
