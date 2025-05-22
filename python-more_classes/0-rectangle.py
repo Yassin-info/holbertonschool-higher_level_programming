d149d35b36f80ca25f0dfa327ea22df2d698ad3f
yassin74 <yassin.jaghmim@gmail.com>
2025-05-22 07:59:36 +0000

    add doc

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+This module provides the Rectangle class for handling rectangle
+geometric shapes. It supports computing area, perimeter, and
+text-based rendering.
+"""
+
+
 class Rectangle:
+    """
+    Representation of a rectangle.
+
+    """
     def __init__(self):
+        "Initializes an instance of Rectangle class"
         pass
