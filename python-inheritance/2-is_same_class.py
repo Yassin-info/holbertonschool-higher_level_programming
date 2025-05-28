c7671a1bbb9a4c8b9cb9c487e9c7964c948488e9
yassin74 <yassin.jaghmim@gmail.com>
2025-05-28 07:44:44 +0000

    fix code

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,20 +4,19 @@ Module that returns the list of available attributes and methods of an object.
 """
 
 
+#!/usr/bin/python3
+"""Module that defines an is_same_class function"""
+
+
 def is_same_class(obj, a_class):
-    """Check if an object is an instance of a given class.
-
-    This function returns True if `obj` is an instance of `a_class` (including subclasses),
-    and False otherwise.
-
+    """Returns True if the object is exactly an instance of the specified class;
+    otherwise False
+    
     Args:
-        obj (Any): The object to check.
-        a_class (type): The class to compare against.
-
+        obj: The object to check
+        a_class: The class to check against
+        
     Returns:
-        bool: True if `obj` is an instance of `a_class`, False otherwise.
+        bool: True if obj is exactly an instance of a_class, False otherwise
     """
-    if not isinstance(obj, a_class):
-        return False
-    else:
-        return True
+    return type(obj) is a_class
