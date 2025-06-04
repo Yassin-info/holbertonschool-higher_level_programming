d44194a54a8f6a4801b994532ffea5c627bb5ec6
yassin74 <yassin.jaghmim@gmail.com>
2025-06-04 09:38:12 +0000

    fix the pycodestyle

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Writes an Object to a text file, using a JSON representation
-    
+
     Args:
         my_obj: Python object to serialize to JSON
         filename (str): Name of the file to write to
