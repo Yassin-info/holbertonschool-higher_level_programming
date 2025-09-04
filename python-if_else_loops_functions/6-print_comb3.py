ec1fe72ef30ee2d6a72375373de0354d49d099f1
yassin74 <yassin.jaghmim@gmail.com>
2025-09-04 23:25:52 +0000

    fix task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
+        if i != 8 or j != 9:
             print("{:01d}{:01d}".format(i, j), end=", ")
+        else:
+            print("{:01d}{:01d}".format(i, j))
