2a1db892edff31bd878c3624f90b9a3e94b445e9
yassin74 <yassin.jaghmim@gmail.com>
2025-09-09 13:04:53 +0000

    finished task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list == []:
-            return (0, None)
+            return None
     max_val = my_list[0]
     for i in my_list:
         if max_val < i:
