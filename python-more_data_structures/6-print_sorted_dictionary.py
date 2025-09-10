b1f621303fe0f1e45ad6a172cf0cb1205276a3c5
yassin74 <yassin.jaghmim@gmail.com>
2025-09-10 08:54:54 +0000

    finished task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new_a_dictionnary = {}
-    for i in a_dictionary:
-        sorted(a_dictionary)
-    new_a_dictionnary = a_dictionary
-    return new_a_dictionnary
+    for keys, values in sorted(a_dictionary.items()):
+        print(f"{keys}: {values}")
