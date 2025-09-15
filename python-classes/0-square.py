1a8bb14131a00de6be7281e7b2af4b93ff4b3f99
yassin74 <yassin.jaghmim@gmail.com>
2025-09-15 09:18:14 +0000

    add the init

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,13 +5,15 @@ This module defines a Square class used to represent a square shape.
 Classes:
     Square: Represents a square with a private size attribute.
 """
+
+
 class Square:
- """
+    """
     Represents a square.
 
     Attributes:
         __size (int): The size of the square (private).
     """
- def __init__(Yassin):
-  "Initializes an instance of square class"
-  pass
+    def __init__(self):
+        "Initializes an instance of square class"
+        pass
