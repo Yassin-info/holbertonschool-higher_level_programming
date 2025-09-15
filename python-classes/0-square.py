53991fa8020a50881f3f54ec04920858811ff772
yassin74 <yassin.jaghmim@gmail.com>
2025-09-15 09:14:59 +0000

    add com for yask 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class used to represent a square shape.
+
+Classes:
+    Square: Represents a square with a private size attribute.
+"""
 class Square:
-    pass
+ """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+ pass
