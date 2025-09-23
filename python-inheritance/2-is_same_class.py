2881f046e44fb32359f59d8b9d0135e93fb870b1
yassin74 <yassin.jaghmim@gmail.com>
2025-09-23 14:22:48 +0000

    fixed pycodestyle

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """Exact same object checker."""
+
+
 def is_same_class(obj, a_class):
+
+
     """Return True if obj is exactly an instance of a_class; else False."""
+
+
     return type(obj) is a_class
