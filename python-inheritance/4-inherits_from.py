6d5711cadb9dc992cd4020664709334b01e6b658
yassin74 <yassin.jaghmim@gmail.com>
2025-09-24 09:30:52 +0000

    fix task 4 with type obj

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     inherited (directly or indirectly) from a_class.
     Otherwise, return False.
     """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
