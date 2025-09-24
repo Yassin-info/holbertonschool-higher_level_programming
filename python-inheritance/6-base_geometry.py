b562e98541448006b5b3de83d2f4754ee5f75bf3
yassin74 <yassin.jaghmim@gmail.com>
2025-09-24 09:45:39 +0000

    finished task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines the area method for BaseGeometry.
+"""
+
+
+def area(self):
+    """Raise an exception indicating that area()
+    is not implemented.
+    """
+    raise Exception("area() is not implemented")
